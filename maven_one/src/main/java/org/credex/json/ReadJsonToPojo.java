ecf7a117d095a55dcca99fcb001700338f1843fb
Prateek Goyal <akshat.goyal_cs17@gla.ac.in>
2021-08-03 12:50:03 +0530

    JDBC_Json Project

---
+++ maven_one/src/main/java/org/credex/json/ReadJsonToPojo.java
@@ -0,0 +1,21 @@
+package org.credex.json;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.PreparedStatement;
+import org.credex.pojo.Employees;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+public class ReadJsonToPojo {
+	
+	private final static String JSON_PATH = "E:\\cp\\employ.json";
+	
+	public static Employees mapJsonUsingJackson() throws Exception {
+		 ObjectMapper mapper = new ObjectMapper();
+         //Parsing the contents of the JSON file
+    	  Employees emp = mapper.readValue(new File(JSON_PATH), Employees.class);
+		return emp;
+	}
+}
